21923860b4a42d367a7591df01e864140424304d
rahim2034 <mdrahimahmmed8080@gmail.com>
2025-07-10 19:00:26 +0600

    Update king.py

--- king.py
+++ king.py
@@ -1293,3 +1293,42 @@ class Main:
 				break
 			else:
 				continue
+
+		self.loop +=1
+
+
+def Subscraption():
+	key1=open('/data/data/com.termux/files/usr/bin/.mrkausar-cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://github.com/rahim2034/Rahim-King/blob/main/Rahim.Txt").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m Rahim Tool Paid You Need Get Approved First\033[1;37m\n")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+ahsan+key1)
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+		os.system('am start https://wa.me/+8801831355810?text=' + tks)
+		Subscraption()        
+Subscraption()
+
+		  
