9811bf001bdbbdcf464f016417bbeb3f8b84d451
rahim2034 <mdrahimahmmed8080@gmail.com>
2025-07-30 14:31:01 +0600

    Update king.py

--- king.py
+++ king.py
@@ -232,21 +232,7 @@ for xd in range(10000):
    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
    ugen.append(uaku2)
    
-   for xd in range(10000):	
-   aa='Mozilla/5.0 (Linux; Android 11;'
-   b=random.choice(['7.0','8.1.0','9','10','11','12'])
-   c=random.choice(['M2012K11AG Build/'])
-   d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-   e=random.randrange(1, 999)
-   f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-   g='RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko)'
-   h=random.randrange(80,103)
-   i='0'
-   j=random.randrange(4200,4900)
-   k=random.randrange(40,150)
-   l='Version/4.0 Chrome/102.0.5005.125 Mobile Safari/537.36 WpsMoffice/16.3.2/arm64-v8a/1328'
-   uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
-   ugen.append(uaku2)
+   
 for xd in range(10000): 	
    aa='Mozilla/5.0 (Linux; Android 11;'
    b=random.choice(['7.0','8.1.0','9','10','11','12'])
