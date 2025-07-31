a07450aa1788397bb2a1c5efd8a57ba304678e4f
rahim2034 <mdrahimahmmed8080@gmail.com>
2025-07-31 12:53:15 +0600

    Update king.py

--- king.py
+++ king.py
@@ -120,7 +120,7 @@ def reg():
         to = open('/sdcard/Android/.bs7nt.txt', 'r').read()
     except (KeyError, IOError):
         reg2()
-    r = requests.get('https://github.com/rahim2034/Rahim-King/').text
+    r = requests.get('https://github.com/rahim2034/Rahim.Txt').text
     if to in r:
         time.sleep(2)
         bsn_menu()
